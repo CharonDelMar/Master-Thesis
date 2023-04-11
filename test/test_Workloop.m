578f3a66459c80c7a713e6a18bbacd44565ff201
CharonDelMar <63898147+CharonDelMar@users.noreply.github.com>
2023-04-11 19:43:03 +0200

    first trial to construct a work-loop
    amplitude x0 = 1
    damping ratio zeta = 0.8
    natural frequency omega = 1
    Frequency is set to be 0:0.1:1 * omega and 0:0.25:1 * omega for work-loop plot
    T = 4 * pi(if i didn't remember it wrong)

---
+++ test/test_Workloop.m
@@ -0,0 +1,45 @@
+%Initialize the Parameters
+T = 10 * pi ;
+
+ampl = 1;
+damping = 0.8;
+natural_freq = 1;
+freq = (0:0.25:1) * natural_freq;
+
+%Construct the system
+%syms x(t) x0 Omega
+%x(t) = x0 * cos(Omega * t);
+
+syms x(t) zeta omega x0 Omega
+x(t) = x0 * cos(Omega * t);
+p = diff(x,t) * (diff(x,t,t) + 2 * zeta * omega * diff(x) + omega^2 * x);
+
+p(t,zeta,omega,x0,Omega) = p;
+
+%fplot(@(t)p(t,damping,natural_freq,ampl,freq(1)),[0 T])
+%hold on;
+%for k = 2:numel(freq)
+%    fplot(@(t)p(t,damping,natural_freq,ampl,freq(k)),[0 T])
+%end
+%hold off;
+%grid on;
+%xticks(T*linspace(0,1,5));
+%xticklabels({'0','\pi','2\pi','3\pi','4\pi'});
+%xlabel('t / \omega');
+%ylabel('f');
+%lgd = legend('0','0.1\omega','0.2\omega','0.3\omega','0.4\omega','0.5\omega','0.6\omega','0.7\omega','0.8\omega','0.9\omega','\omega');
+%title(lgd,'\Omega');
+%title('p-t');
+
+x(t,x0,Omega) = x;
+fplot(@(t)x(t,ampl,freq(1)),@(t)p(t,damping,natural_freq,ampl,freq(1)),[0 T])
+hold on;
+for k = 2:numel(freq)
+    fplot(@(t)x(t,ampl,freq(k)),@(t)p(t,damping,natural_freq,ampl,freq(k)),[0 T])
+end
+hold off;
+grid on;
+lgd = legend('0','0.25\omega','0.5\omega','0.75\omega','\omega');
+%lgd = legend('0','0.1\omega','0.2\omega','0.3\omega','0.4\omega','0.5\omega','0.6\omega','0.7\omega','0.8\omega','0.9\omega','\omega');
+title(lgd,'\Omega');
+title('Work loop');
